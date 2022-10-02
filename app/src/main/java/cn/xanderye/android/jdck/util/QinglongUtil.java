3043c21c885a839d920c0c29289b6c0730771b01
悠悠是梦 <radaig@163.com>
2022-10-02 14:26:08 +0800

    Update QinglongUtil.java

--- app/src/main/java/cn/xanderye/android/jdck/util/QinglongUtil.java
+++ app/src/main/java/cn/xanderye/android/jdck/util/QinglongUtil.java
@@ -36,7 +36,7 @@ public class QinglongUtil {
         //JSONObject params = new JSONObject();
         //params.put("username", qlInfo.getUsername());
         //params.put("password", qlInfo.getPassword());
-        Map<String, String> querys = new HashMap<String, String>();
+        Map<String, String> querys = new HashMap<String, Object>();
         querys.put("client_id",qlInfo.getUsername());
         querys.put("client_secret",qlInfo.getPassword());
         
